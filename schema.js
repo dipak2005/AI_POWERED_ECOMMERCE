dfe137c48aae798914fcc04326537ec0ff2ecc07
VIRAMGAMA PINAK <pinakdv11@gmail.com>
2025-04-06 10:49:49 +0530

    Revert "UI Modifications - Enrollment 75"

--- schema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi"); // Use uppercase 'Joi'
-
-const reviewSchema = Joi.object({
-  rating: Joi.number().required(),
-  comment: Joi.string().required(),
-});
-
-module.exports = reviewSchema; // Export directly
